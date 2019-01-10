2edb4663585d67a648e74c681d9d68aa767beb32
Matt Dickey <dickey@vermilion.com>
2019-01-09 17:30:14 -0700

    switched docs and demo folders

--- docs/prism.02026305.js
+++ docs/prism.02026305.js
@@ -171,7 +171,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../../../usr/local/lib/node_modules/parcel/src/builtins/bundle-url.js"}],"external-styles.scss":[function(require,module,exports) {
+},{"./bundle-url":"../../../../../../usr/local/lib/node_modules/parcel/src/builtins/bundle-url.js"}],"lib/prism.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52245" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54450" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
